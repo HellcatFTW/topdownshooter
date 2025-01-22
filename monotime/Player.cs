690ce27f3d9a8ac6d4a7ef564d15fd600f56ba4b
HellcatFTW <devcatftw@gmail.com>
2025-01-22 10:58:52 +0300

    fixed weird player draw offsets

--- monotime/Player.cs
+++ monotime/Player.cs
@@ -10,9 +10,6 @@ namespace TopDownShooter
 		private readonly Texture2D TankHull;
         private readonly Texture2D TankTurret;
 
-		private Vector2 HullPos;
-		private Vector2 TurretPos;
-
 		private Vector2 directionToMouse;
 		private float turretRotation = 0f;
 		private float hullRotation = 0f;
@@ -33,25 +30,22 @@ namespace TopDownShooter
 		{
             MouseState mouse = Mouse.GetState();
 
-            Vector2 turretOrigin = new Vector2(TankTurret.Width / 2, TankTurret.Height / 2 + 9f);
-			Vector2 hullOrigin = new Vector2(TankHull.Width / 2, TankTurret.Height / 2);
+            if (inputAxis != Vector2.Zero)
+            {
+                hullRotation = (float)System.Math.Atan2(inputAxis.Y, -inputAxis.X) - MathHelper.PiOver2;
+            }
 
-			HullPos = position + hullOrigin;
-			TurretPos = position + turretOrigin; // + new Vector2(0, -4f);
+            Vector2 turretOrigin = new Vector2(TankTurret.Width / 2, TankTurret.Height / 2 + 9f); // 9 pixel offset to accommodate for the barrel
+            Vector2 hullOrigin = new Vector2(TankHull.Width / 2, TankTurret.Height / 2);
 
-            directionToMouse = new Vector2(mouse.X - TurretPos.X, mouse.Y - TurretPos.Y);
+            directionToMouse = new Vector2(mouse.X - position.X, mouse.Y - position.Y);
             turretRotation = (float)System.Math.Atan2(directionToMouse.Y, directionToMouse.X);
             turretRotation += MathHelper.PiOver2;
 			
 
-			if (inputAxis != Vector2.Zero)
-			{
-				hullRotation = (float)System.Math.Atan2(inputAxis.Y, -inputAxis.X) - MathHelper.PiOver2;
-			}
+            Globals.SpriteBatch.Draw(TankHull, position, null, Color.White, hullRotation, hullOrigin, 1, SpriteEffects.None, 0);
 
-            Globals.SpriteBatch.Draw(TankHull, HullPos, null, Color.White, hullRotation, hullOrigin, 1, SpriteEffects.None, 0);
-
-			Globals.SpriteBatch.Draw(TankTurret, TurretPos, null, Color.White, turretRotation, turretOrigin, 1, SpriteEffects.None, 0);
+			Globals.SpriteBatch.Draw(TankTurret, position, null, Color.White, turretRotation, turretOrigin, 1, SpriteEffects.None, 0);
 		}
 	}
 }
