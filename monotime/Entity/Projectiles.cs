e30021017e80af09fea6fcd25c8827715228a087
HellcatFTW <devcatftw@gmail.com>
2025-04-30 01:44:47 +0300

    - Removed useless field and lines from projectiles

--- monotime/Entity/Projectiles.cs
+++ monotime/Entity/Projectiles.cs
@@ -11,13 +11,11 @@ namespace TopDownShooter.Entities
         private float damage;
         public override bool IsHostile { get => isHostile; }
         private bool isHostile;
-        private Texture2DAnimated impactFX;
         public PlayerShell() : base(Globals.Content.Load<Texture2D>("PlayerShell"))
         {
             Speed = 10f;
             damage = 50f;
             isHostile = false;
-            impactFX = new Texture2DAnimated(Globals.Content.Load<Texture2D>("ShellImpactFX"), 4, 10, false);
         }
 
         public override void Update()
@@ -29,7 +27,6 @@ namespace TopDownShooter.Entities
         public override void Kill(Vector2 impactNormal, Vector2 startPoint, bool FlipMTVWhenDrawing, WeakReference<Entity> parentWeakRef)
         {
             float impactFXRotation = impactNormal.ToRotation() + MathHelper.PiOver2 + (FlipMTVWhenDrawing ? 0 : MathHelper.Pi);
-            Vector2 origin = new Vector2(impactFX.Width / 2, impactFX.Height / 2);
             Effect.NewEffect(startPoint, impactFXRotation, 1f, parentWeakRef);
             base.Kill(impactNormal, startPoint, FlipMTVWhenDrawing);
         }
@@ -42,13 +39,11 @@ namespace TopDownShooter.Entities
         private float damage;
         public override bool IsHostile { get => isHostile; }
         private bool isHostile;
-        private Texture2D impactFX;
         public EnemyShell() : base(Globals.Content.Load<Texture2D>("EnemyShell"))
         {
             Speed = 5f;
             damage = 25f;
             isHostile = true;
-            impactFX = Globals.Content.Load<Texture2D>("ShellImpactFX");
         }
         public override void Update()
         {
@@ -59,7 +54,6 @@ namespace TopDownShooter.Entities
         public override void Kill(Vector2 impactNormal, Vector2 startPoint, bool FlipMTVWhenDrawing, WeakReference<Entity> parentWeakRef)
         {
             float impactFXRotation = impactNormal.ToRotation() + MathHelper.PiOver2 + (FlipMTVWhenDrawing ? 0 : MathHelper.Pi);
-            Vector2 origin = new Vector2(impactFX.Width / 2, impactFX.Height / 2);
             Effect.NewEffect(startPoint, impactFXRotation, 1f, parentWeakRef);
             base.Kill(impactNormal, startPoint, FlipMTVWhenDrawing);
         }
